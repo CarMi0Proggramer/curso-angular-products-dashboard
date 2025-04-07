efcccc0ac9c1a2a9bfcf9919d4fc2407b38cee28
Carlos Miguel <carlosmiguelgarcesbasulto@gmail.com>
2025-04-07 05:08:09 -0400

    Update app structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,64 +1,10 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CreateProductModalComponent } from './create-product-modal/create-product-modal.component';
-import { UpdateProductModalComponent } from './update-product-modal/update-product-modal.component';
-import { ConfirmDeleteModalComponent } from './confirm-delete-modal/confirm-delete-modal.component';
-import { FilterDropdownComponent } from './filter-dropdown/filter-dropdown.component';
-import { ProductsService } from './core/services/products.service';
-import { Product } from './core/interfaces/product';
-import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    CreateProductModalComponent,
-    UpdateProductModalComponent,
-    ConfirmDeleteModalComponent,
-    FilterDropdownComponent,
-    CurrencyPipe,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
-  private readonly productsService = inject(ProductsService);
-  protected products = signal<Product[]>([]);
-
-  /*
-  TODO: Inicializar los eventos para mostrar los componentes interactivos usando flowbite
-
-  * Se debe elegir el momento del ciclo de vida adecuado
-  */
-  ngOnInit(): void {
-    this.productsService.getAll().subscribe((products) => {
-      this.products.set(products);
-
-      setTimeout(() => {
-        import('flowbite').then((flowbite) => {
-          flowbite.initFlowbite();
-        });
-      }, 200);
-    });
-  }
-
-  onProductCreated(product: Product) {
-    this.products.update((products) => [product, ...products]);
-  }
-
-  onProductUpdated(updatedProduct: Product) {
-    this.products.update((products) => {
-      const index = products.findIndex(
-        (product) => product.id === updatedProduct.id
-      );
-      products[index] = updatedProduct;
-      return products;
-    });
-  }
-
-  onProductDeleted(product: Product) {
-    this.products.update((products) =>
-      products.filter((p) => p.id !== product.id)
-    );
-  }
-}
+export class AppComponent {}
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,56 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ConfirmDeleteModalComponent } from '../confirm-delete-modal/confirm-delete-modal.component';
+import { Product } from '../core/interfaces/product';
+import { ProductsService } from '../core/services/products.service';
+import { CreateProductModalComponent } from '../create-product-modal/create-product-modal.component';
+import { FilterDropdownComponent } from '../filter-dropdown/filter-dropdown.component';
+import { UpdateProductModalComponent } from '../update-product-modal/update-product-modal.component';
+
+@Component({
+  selector: 'app-products',
+  imports: [
+    CreateProductModalComponent,
+    UpdateProductModalComponent,
+    ConfirmDeleteModalComponent,
+    FilterDropdownComponent,
+    CurrencyPipe,
+  ],
+  templateUrl: './products.component.html',
+})
+export class ProductsComponent implements OnInit {
+  private readonly productsService = inject(ProductsService);
+  protected products = signal<Product[]>([]);
+
+  ngOnInit(): void {
+    this.productsService.getAll().subscribe((products) => {
+      this.products.set(products);
+
+      setTimeout(() => {
+        import('flowbite').then((flowbite) => {
+          flowbite.initFlowbite();
+        });
+      }, 200);
+    });
+  }
+
+  onProductCreated(product: Product) {
+    this.products.update((products) => [product, ...products]);
+  }
+
+  onProductUpdated(updatedProduct: Product) {
+    this.products.update((products) => {
+      const index = products.findIndex(
+        (product) => product.id === updatedProduct.id
+      );
+      products[index] = updatedProduct;
+      return products;
+    });
+  }
+
+  onProductDeleted(product: Product) {
+    this.products.update((products) =>
+      products.filter((p) => p.id !== product.id)
+    );
+  }
+}
